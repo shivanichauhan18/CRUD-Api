2c51826e192d2bf418c320b462227b62d8912393
shivani chouhan <shivanic18@navgurukul.org>
2019-09-27 16:58:09 +0530

    In this rout I delete particular data by Id

--- requests_routes.js
+++ requests_routes.js
@@ -45,5 +45,22 @@ body.get("/get/:name",function(req,res){
     }
 });
 
+// deleting data
+
+body.delete("/delete/:id",function(req,res){
+    id=req.params.id
+    data=fs.readFileSync("courses.json")
+    var Data=JSON.parse(data);
+    for (var i in Data){
+        if(Data[i]["id"]==id){
+            delete Data[i]
+            fs.writeFileSync("courses.json",JSON.stringify(Data,null,2))
+            console.log(Data)
+            res.json(Data)
+        };
+    };
+});
+
+
 
 app.listen(8000, () => console.log('server is listening'));
